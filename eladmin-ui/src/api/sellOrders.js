ec31e4303487073e54397f24301e62c7aed48840
Bin_yoo <mapconn@live.com>
2022-01-28 18:08:56 +0800

    销售退货单模块前端

---
+++ eladmin-ui/src/api/sellOrders.js
@@ -0,0 +1,106 @@
+import request from '@/utils/request'
+
+export function add(data) {
+  return request({
+    url: 'api/sellOrders',
+    method: 'post',
+    data
+  })
+}
+
+export function del(ids) {
+  return request({
+    url: 'api/sellOrders/',
+    method: 'delete',
+    data: ids
+  })
+}
+
+export function edit(data) {
+  return request({
+    url: 'api/sellOrders',
+    method: 'put',
+    data
+  })
+}
+
+export function getCustomerSelect() {
+  return request({
+    url: 'api/sellOrders/getCustomerSelect',
+    method: 'get'
+  })
+}
+
+export function getManagerSelect() {
+  return request({
+    url: 'api/sellOrders/getManagerSelect',
+    method: 'get'
+  })
+}
+
+export function getOrderNo() {
+  return request({
+    url: 'api/sellOrders/getOrderNo',
+    method: 'get'
+  })
+}
+
+export function getGoodChooseList(params) {
+  return request({
+    url: 'api/sellOrders/getGoodChooseList',
+    method: 'get',
+    params
+  })
+}
+
+export function getOrderGoodList(id) {
+  return request({
+    url: 'api/sellOrders/getOrderGoodList?id=' + id,
+    method: 'get'
+  })
+}
+
+export function approveOrders(data) {
+  return request({
+    url: 'api/sellOrders/approveOrders',
+    method: 'put',
+    data
+  })
+}
+
+export function reApproveOrders(data) {
+  return request({
+    url: 'api/sellOrders/reApproveOrders',
+    method: 'put',
+    data
+  })
+}
+
+export function printOrderReport(id) {
+  return request({
+    url: 'api/sellOrders/printOrderReport?id=' + id,
+    method: 'get'
+  })
+}
+
+export function getOrderPrintingInfo(id) {
+  return request({
+    url: 'api/sellOrders/getOrderPrintingInfo?id=' + id,
+    method: 'get'
+  })
+}
+
+export default {
+  add,
+  edit,
+  del,
+  getCustomerSelect,
+  getManagerSelect,
+  getOrderNo,
+  getGoodChooseList,
+  getOrderGoodList,
+  approveOrders,
+  reApproveOrders,
+  printOrderReport,
+  getOrderPrintingInfo
+}
