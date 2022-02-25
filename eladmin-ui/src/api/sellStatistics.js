b156f466f34d22caee1326e79584196385cc9b62
Bin_yoo <mapconn@live.com>
2022-02-25 18:24:06 +0800

    货品销售汇总前端

---
+++ eladmin-ui/src/api/sellStatistics.js
@@ -0,0 +1,40 @@
+import request from '@/utils/request'
+
+export function add(data) {
+  return request({
+    url: 'api/sellStatistics',
+    method: 'post',
+    data
+  })
+}
+
+export function del(ids) {
+  return request({
+    url: 'api/sellStatistics/',
+    method: 'delete',
+    data: ids
+  })
+}
+
+export function edit(data) {
+  return request({
+    url: 'api/sellStatistics',
+    method: 'put',
+    data
+  })
+}
+
+export function getDetail(params) {
+  return request({
+    url: 'api/sellStatistics/getDetail',
+    method: 'get',
+    params
+  })
+}
+
+export default {
+  add,
+  edit,
+  del,
+  getDetail
+}
